32e6ce246a03c560a906a5a71286351c0d2ad1db
JesseAnderson13 <122398049+JesseAnderson13@users.noreply.github.com>
2024-12-04 07:19:25 -0500

    Delete scripts directory

--- scripts/generateScript.js
+++
@@ -1,8 +0,0 @@
-let generateButton = document.getElementById("generateBtn");
-generateButton.addEventListener("click", generate);
-
-function generate(){
-	alert("Playlist Generated!")
-	//add functionality for disabling the button
-	//1 day for premium, 1 week for basic
-}
